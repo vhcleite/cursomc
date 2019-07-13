9832c6622b74093c88d9c733d3f8148db72bd147
Victor Leite <victor_leite@outlook.com.br>
2019-07-13 17:38:50 -0300

    Testando a primeira classe de domínio

--- src/main/java/com/vleite/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/vleite/cursomc/resources/CategoriaResource.java
@@ -1,16 +1,29 @@
 package com.vleite.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.vleite.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "O REST está funcionando";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> categorias = new ArrayList<Categoria>();
+		categorias.add(cat1);
+		categorias.add(cat2);
+		
+		return categorias;
 	}
 
 }
