65f56de7a4d4011541bc34139945e1642180a0f4
Victor Leite <victor_leite@outlook.com.br>
2019-07-18 22:38:54 -0300

    Domínio de Produto e associação de muitos para muitos entre Categoria e Produto

--- src/main/java/com/vleite/cursomc/CursomcApplication.java
+++ src/main/java/com/vleite/cursomc/CursomcApplication.java
@@ -1,22 +1,25 @@
 package com.vleite.cursomc;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.core.env.CommandLinePropertySource;
 
 import com.vleite.cursomc.domain.Categoria;
+import com.vleite.cursomc.domain.Produto;
 import com.vleite.cursomc.repositories.CategoriaRepository;
+import com.vleite.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner{
 
 	@Autowired
 	CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	ProdutoRepository produtoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -28,7 +31,19 @@ public class CursomcApplication implements CommandLineRunner{
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
 		
+		Produto p1 = new Produto(null, "computador", 2000.0);
+		Produto p2 = new Produto(null, "impressora", 800.0);
+		Produto p3 = new Produto(null, "mouse", 80.0);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 	}
 
 }
