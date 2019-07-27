c68e78338256fcc603d7fe7e7e1b5b8b39b79db0
Victor Leite <victor_leite@outlook.com.br>
2019-07-27 00:06:53 -0300

    Tratando exceção de entidade não encontrada no end point de busca de categoria

--- src/main/java/com/vleite/cursomc/services/CategoriaService.java
+++ src/main/java/com/vleite/cursomc/services/CategoriaService.java
@@ -7,15 +7,17 @@ import org.springframework.stereotype.Service;
 
 import com.vleite.cursomc.domain.Categoria;
 import com.vleite.cursomc.repositories.CategoriaRepository;
+import com.vleite.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	CategoriaRepository repository;
-	
+
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				String.format("Objeto não encontrado! Id: %d, Tipo: %s", id, Categoria.class.getCanonicalName())));
 	}
 }
