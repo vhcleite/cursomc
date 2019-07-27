d8d0d48369c27141e7647ff3a8b2d4817980ad8f
Victor Leite <victor_leite@outlook.com.br>
2019-07-26 23:17:11 -0300

    Resolvendo Bug de Descerializacao de objetos em tabela de muitos para muitos

--- src/main/java/com/vleite/cursomc/domain/Produto.java
+++ src/main/java/com/vleite/cursomc/domain/Produto.java
@@ -12,6 +12,8 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 @Entity
 public class Produto implements Serializable {
 
@@ -23,8 +25,10 @@ public class Produto implements Serializable {
 	private String nome;
 	private double preco;
 
+	@JsonManagedReference
 	@ManyToMany
-	@JoinTable(name = "PRODUTO_CATEGORIA", joinColumns = @JoinColumn(name = "produto_id"), inverseJoinColumns = @JoinColumn(name = "categoria_id"))
+	@JoinTable(name = "PRODUTO_CATEGORIA", joinColumns = @JoinColumn(name = "produto_id"), 
+	inverseJoinColumns = @JoinColumn(name = "categoria_id"))
 	private List<Categoria> categorias = new ArrayList<Categoria>();
 
 	public Produto() {
