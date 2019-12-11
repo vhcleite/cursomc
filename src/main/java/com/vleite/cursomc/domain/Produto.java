86da59ce0fd07466208e2131badd3ec7769ff463
Victor Leite <victor_leite@outlook.com.br>
2019-12-11 13:32:03 -0300

    Arrumando anotações para problema de loop infinito ao listar categorias e seus produtos

--- src/main/java/com/vleite/cursomc/domain/Produto.java
+++ src/main/java/com/vleite/cursomc/domain/Produto.java
@@ -12,7 +12,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
-import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.JsonBackReference;
 
 @Entity
 public class Produto implements Serializable {
@@ -25,7 +25,7 @@ public class Produto implements Serializable {
 	private String nome;
 	private double preco;
 
-	@JsonManagedReference
+	@JsonBackReference
 	@ManyToMany
 	@JoinTable(name = "PRODUTO_CATEGORIA", joinColumns = @JoinColumn(name = "produto_id"), 
 	inverseJoinColumns = @JoinColumn(name = "categoria_id"))
